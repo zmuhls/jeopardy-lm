ca68c8e60849e1b5c15c2fa2f98960a9c1f81ccb
zmuhls <zmuhlbauer1@gmail.com>
2025-10-14 22:29:53 +0000

    Deploying to gh-pages from @ zmuhls/jeopardy-lm@d3d6ce16585e23497bd2735de228daadb8e7330f 🚀

--- _next/static/L0HZA5PDP28OnH5ymH3ls/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-71110c5a164e628d.js"],"/404":["static/chunks/pages/404-8665a768164d21aa.js"],"/_error":["static/chunks/pages/_error-d13c28027af6a4ac.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
